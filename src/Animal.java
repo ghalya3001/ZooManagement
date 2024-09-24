6d9b40542fe882abe7cb9f132e640538359d2c0a
ghalya3001 <mimounaghalyya@gmail.com>
2024-09-24 23:03:49 +0200

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+    String nom,family;
+    int age;
+    boolean isMammal;
+
+    public Animal(){}
+    public Animal(String family,String nom,int age,boolean isMammal){
+        this.family=family;
+        this.nom=nom;
+        this.age=age;
+        this.isMammal=isMammal;
+    }
+}
