138ed090e09d3b8ae5c97723d279541b45248b3b
ghalya3001 <mimounaghalyya@gmail.com>
2024-11-11 23:58:45 +0100

    Description de vos modifications

--- src/EmployeIdComparator.java
+++ src/EmployeIdComparator.java
@@ -2,9 +2,8 @@ import java.util.Comparator;
 
 public class EmployeIdComparator implements Comparator<employe> {
     @Override
-    public int compare(employe emp1, employe emp2) {
-            return Integer.compare(emp1.getId(), emp2.getId());
-
-        }
+    public int compare(employe e1, employe e2) {
+        return Integer.compare(e1.getId(), e2.getId());
+    }
     }
 
